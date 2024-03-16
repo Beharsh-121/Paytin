7fdf582ff8751cfbb564d9afea58dddd00b15ee9
Harsh Tiwari <123188701+Beharsh-121@users.noreply.github.com>
2024-03-16 22:06:29 +0530

    Update Sidebar.jsx and fixed sidebar card bug

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,20 @@
 import Card from "./Card";
 
+const formatDateTime = (createdAt) => {
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false, // 24-hour format
+  };
+  return new Date(createdAt).toLocaleString(undefined, options);
+};
+
 export default function Sidebar({ array, collapsed, handleToggle }) {
-  const collapsedClassses = `w-22 overflow-x-hidden bg-blue-900 text-stone-50 transition-all duration-500 h-screen`;
+  const collapsedClassses = `w-22 overflow-x-hidden bg-blue-900 text-stone-50 transition-all duration-500 h-auto`;
   const openClasses = `w-1/5 overflow-x-hidden px-8 py-16 bg-blue-900 text-stone-50 md:w-72 rounded-r-xl transition-all duration-500 h-auto `;
 
   return (
@@ -15,7 +28,7 @@ export default function Sidebar({ array, collapsed, handleToggle }) {
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
       {collapsed ? "SIDEBAR" : "Transactions"}
       </h2>
-      {!collapsed && <Card array={array} />}
+      {!collapsed && array && array.map((ar,idx) => <Card key={idx} index={idx} amount={ar.amount} createdAt={formatDateTime(ar.createdAt)} to={ar.to} />)}
     </div>
   );
 }
