08d062e6ab24c95c70b0ce725d33d124501d566b
Harsh Tiwari <123188701+Beharsh-121@users.noreply.github.com>
2024-03-16 22:04:13 +0530

    Update Card.jsx and removed cards bug

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,39 +1,26 @@
 import React from "react";
 
-const formatDateTime = (createdAt) => {
-  const options = {
-    year: "numeric",
-    month: "short",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false, // 24-hour format
-  };
-  return new Date(createdAt).toLocaleString(undefined, options);
-};
 
-const Card = ({ array, toggle }) => {
+
+const Card = ({ index, amount, createdAt, to }) => {
   return (
     <div className="card bg-slate-200  text-blue-800 rounded-xl m-3 overflow-x-hidden">
-      {array &&
-        array.map((ar, idx) => (
-          <div className="rounded shadow-lg px-4  py-2" key={idx}>
+          <div className="rounded shadow-lg px-4  py-2">
             <div className="flex justify-center">
               <div className="font-bold text-xl mr-2">Payment</div>
               <span className=" bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-900 mr-2 mb-2">
-                #{idx + 1}
+                #{index + 1}
               </span>
             </div>
 
             <p className="text-white-700 text-base">
-              Transaction of amount {ar.amount} on Date and Time{" "}
-              {formatDateTime(ar.createdAt)} to {ar.to}
+              Transaction of amount {amount} on Date and Time{" "}
+              {createdAt} to {to}
             </p>
           </div>
-        ))}
     </div>
   );
 };
 
 export default Card;
+
